c0eb2c797e84da069eed96051dfb2ec810702bd0
Guy <me@garyallin.uk>
2020-11-29 14:18:45 +0000

    mail config

--- public/assets/sendme.php
+++ public/assets/sendme.php
@@ -5,24 +5,14 @@ if(isset($postdata) && !empty($postdata)) {
     $request = json_decode($postdata, true);   
      $name = $request['name'];
      $email = $request['email'];
-     $message = $request['message'];
+     $message = "This message is only visible to the FisicalFitness.co.uk It is a contact request message from your website.";
 
-     $to = 'zach@fisicalfitness.co.uk';
-     $to = 'me@garyallin.uk';
-     $subject = 'Contact Request';     
-     $headers = 'From:'.$name;
-     $headers .= 'Reply-To:'.$email;
+    
+     $to = "me@garyallin.uk, zach@fisicalfitness.co.uk";
+     $subject = "Contact Request";     
+     $headers = "From:".$name."\r\n";
+     $headers .= "Reply-To:".$email;
 
-    mail($to, $subject, $message, $headers);
-
-//     //Example with StdClass
-// $json = '{"name":"Gary Allin","email":"gary.allin@gmail.com","message":"hhyhhy"}';
-// $stdInstance = json_decode($json);
-// echo $stdInstance->name . PHP_EOL; //"bar"
-// echo $stdInstance->email . PHP_EOL; //42
-// //Example with associative array
-// $array = json_decode($json, true);
-// echo $array['name'] . PHP_EOL; //"bar"
-// echo $array['email'] . PHP_EOL; //42
+    mail( $to, $subject, $message, $headers);
 }
 ?>
